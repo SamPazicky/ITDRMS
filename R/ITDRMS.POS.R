00fedaf8f342c534c4542be60a244ca86f1914c1
Pazicky Samuel <samuel.pazicky@staff.main.ntu.edu.sg>
2024-10-21 13:59:04 +0800

    Resolving clash and added POI option in ITDRMS.hit

--- R/ITDRMS.POS.R
+++ R/ITDRMS.POS.R
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-#' ITDRMS.plot
-#'
-#' Identifies hits from fitted mass spec data.
-#' @param protein Character string: Identifier of a protein that can be found in 'id' column of source.data.
-#' @param source.data Data frame with measured melting curve, with one column for protein 'id' and other columns for scaled(zero to one) values
-#' @param temperatures vector: for which temperatures should POS be calculated? Default is c(50,55,60)
-#' @return Data frame with calculated plausibility values.
-#' @examples 
-#' plausibilities <- ITDRMS.plot("PF3D7_1017500", ITDRMS::source.data.lysate)
-#' @export
-
-ITDRMS.POS <- function(
-    protein=NA,
-    source.data=Pf_meltcurve_lysate,
-    temperatures=c(50,55,60) # temperatures to be assessed
-) {
-  
-  if(is.na(protein)) {
-    stop("Input protein name.")
-  }
-  
-  protein_reference <- source.data %>%
-    as.data.frame() %>%
-    filter(id==protein) %>%
-    pivot_longer(cols=!id, names_to="temperature",values_to="value") %>%
-    dplyr::select(!id) %>%
-    mutate(temperature=as.numeric(str_remove(temperature,"X")))
-  
-  if(nrow(protein_reference)==0) {
-    plausabilities <- data.frame("Temperature"=temperatures,
-                                 "Stabilization plausability"=rep(NA,length(temperatures)),
-                                 "Destabilization plausability"=rep(NA,length(temperatures)),
-                                 "SD"=rep(NA,length(temperatures))
-    )
-  } else {
-    
-    plausabilities <- data.frame()
-    data <- protein_reference %>% setNames(c("x","y"))%>%na.omit()
-    fit <- fit_MC(data)
-    
-    if(class(fit)=="try-error") {
-     
-      plausabilities <- bind_rows(plausabilities,
-                                  data.frame("Temperature"=temperatures,
-                                             "Prediction"=rep(NA,length(temperatures))
-                                  )
-      )
-    } else {
-      
-      prediction <- predict(fit, data.frame(x=temperatures))
-      
-      plausabilities <- bind_rows(plausabilities,
-                                  data.frame("Temperature"=temperatures,
-                                             "Prediction"=prediction
-                                  )
-      )
-    }
-      
-    plausabilities <- plausabilities %>%
-      group_by(Temperature) %>%
-      dplyr::summarise(mean_pred=mean(Prediction,na.rm=TRUE)) %>%
-      mutate(op_mean_pred=1-mean_pred) %>%
-      dplyr::select(Temperature, op_mean_pred,mean_pred) %>%
-      setNames(c("Temperature","Stabilization.plausability", "Destabilization.plausability"))
-  }
-  
-  return(plausabilities)
-
-}
-=======
 #' ITDRMS.plot
 #'
 #' Identifies hits from fitted mass spec data.
@@ -138,4 +67,3 @@ ITDRMS.POS <- function(
   return(plausabilities)
 
 }
->>>>>>> 2448b81 (Update)
--- R/Pf_meltcurve_lysate.R
+++ R/Pf_meltcurve_lysate.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #' Plasmodium falciparum lysate melting curves
 #'
 #' The melting curves were recorded at 34 hpi synchronized culture
@@ -15,22 +14,4 @@
 #' @examples
 #' data(Pf_meltcurve_lysate)
 #' ITDRMS.POS("O14818",Pf_meltcurve_lysate)
-=======
-#' Plasmodium falciparum lysate melting curves
-#'
-#' The melting curves were recorded at 34 hpi synchronized culture
-#' in lysate setup.
-#'
-#' @docType data
-#'
-#' @usage data(Pf_meltcurve_lysate)
-#' 
-#' @references Pazicky et al. (2024) Unpublished
-#' (\href{tbd}{PubMed})
-#'
-
-#' @examples
-#' data(Pf_meltcurve_lysate)
-#' ITDRMS.POS("O14818",Pf_meltcurve_lysate)
->>>>>>> 2448b81 (Update)
 "Pf_meltcurve_lysate"
