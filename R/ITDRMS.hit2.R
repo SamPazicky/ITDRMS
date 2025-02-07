68c085d6500a656822880c80f447e7f5ff9641ca
Pazicky Samuel <samuel.pazicky@staff.main.ntu.edu.sg>
2025-02-07 15:21:13 +0800

    Adjusted plotting in ITDRMS.hit2

--- R/ITDRMS.hit2.R
+++ R/ITDRMS.hit2.R
@@ -238,13 +238,8 @@ ITDRMS.hit2 <- function(
     }
   }
   
-  hit_plot <- hit_data %>% ggplot(aes(total.response, -log10(CI)))
-  if(secondline) {
-    hit_plot <- hit_plot +
-      geom_hline(yintercept = -log10(CIthreshold[2]), linetype = "dashed", color = "gray80") 
-  }
-  hit_plot <- hit_plot +
-    geom_hline(yintercept = -log10(CIthreshold[1]), linetype = "dashed", color = "gray80") +
+  hit_plot <- hit_data %>% ggplot(aes(total.response, -log10(CI))) %>%
+    geom_hline(yintercept = -log10(ifelse(secondline,CIthreshold[2],CIthreshold[1])), linetype = "dashed", color = "gray80") +
     geom_vline(xintercept = min(minresponse), linetype = "dashed", color = "gray80") +
     geom_vline(xintercept = (-1) * min(minresponse), linetype = "dashed", color = "gray80") +
     geom_point(aes(
